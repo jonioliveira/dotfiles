10df115ff6aabadf35a4c23ab743d12910789a00
Jóni Oliveira <joni.oliveira89@gmail.com>
2019-09-23 12:01:35 +0100

    Add dotfiles

---
+++ brew.sh
@@ -0,0 +1,15 @@
+brew update 
+
+brew upgrade
+
+brew install git
+brew install node  
+
+brew cask install spectacle
+brew cask install google-chrome
+brew cask install slack
+brew cask install iterm2
+brew cask install mysqlworkbench
+brew cask install sequel-pro
+
+brew cleanup
